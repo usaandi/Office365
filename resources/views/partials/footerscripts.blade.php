c3001389751663b916a0523f6d8a9ce730180fc9
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-06-20 17:02:40 +0300

    footerscripts

--- resources/views/partials/footerscripts.blade.php
+++ resources/views/partials/footerscripts.blade.php
@@ -1,13 +1,13 @@
 <!-- begin::Quick Nav -->
 <!--begin::Base Scripts -->
-<script src="assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
-<script src="assets/demo/demo8/base/scripts.bundle.js" type="text/javascript"></script>
+<script src="{{asset('assets/vendors/base/vendors.bundle.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/demo/demo8/base/scripts.bundle.js')}}" type="text/javascript"></script>
 <!--end::Base Scripts -->
 <!--begin::Page Vendors -->
-<script src="assets/vendors/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
+<script src="{{asset('assets/vendors/custom/fullcalendar/fullcalendar.bundle.js')}}" type="text/javascript"></script>
 <!--end::Page Vendors -->
 <!--begin::Page Snippets -->
-<script src="assets/app/js/dashboard.js" type="text/javascript"></script>
+<script src="{{asset('assets/app/js/dashboard.js')}}" type="text/javascript"></script>
 <!--end::Page Snippets -->
 <!-- begin::Page Loader -->
 <script>
