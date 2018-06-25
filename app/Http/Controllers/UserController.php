d379dbfa5e4a0809c18dab88ddde5e9705b3639d
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-06-25 15:22:22 +0300

    Update user data in website

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,43 @@ class UserController extends Controller
         $user=User::findorFail($id);
 
         return view('user.profileview',compact('user'));
+        //->with('user',$user);
 //
+    }
+    public function showedit($id) {
+
+        $user=User::findorFail($id);
+        return view('user.userupdate',compact('user'));
+    }
+
+    public function update(Request $request, $id){
+
+        $request->validate([
+
+                'name'=>'',
+                'email'=> 'email',
+                'phone'=> 'int',
+                'birthday'=> 'date',
+                'skype'=>'',
+                'team'=>'',
+                'ADMsince'=>'date',
+            ]);
+        $name=$request->input('name');
+        $email=$request->input('email');
+        $phone=$request->input('phone');
+        $birthday=$request->input('birthday');
+        $skype=$request->input('skype');
+        $team=$request->input('team');
+        $ADMsince=$request->input('ADMsince');
+
+        $user = User::findOrFail($id);
+
+        $user->update(['email' => $email,'name'=>$name,'phoneN'=>$phone,
+            'birthday'=>$birthday, 'skype'=>$skype, 'team'=>$team,'ADMsince'=>$ADMsince]);
+        $user->save();
+
+        return view('user.userupdate',compact('user'));
+
     }
 
 
