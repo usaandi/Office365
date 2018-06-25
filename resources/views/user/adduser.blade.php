817236c15d5cba2b74383ecf6e2ea4c54a4d89bd
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-06-25 15:27:46 +0300

    finished View to add users into database

--- resources/views/user/adduser.blade.php
+++ resources/views/user/adduser.blade.php
@@ -25,17 +25,50 @@
 @endsection
 
 @section('content-body')
+ <style>
+     input, select, textarea{
+         width:200px;
+         border:1px solid #babacc;
+         box-sizing:border-box;
 
+     }
+     label {
+         width: 100px;
+     }
+ </style>
     <form name="form" action="{{route('store')}}" method="post">
         @csrf
-
-        <label for="uname"><b>Username</b></label>
+<div class="card-body">
+        <h1>Lisa kasutaja</h1>
+        <label for="name"><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="name" value="" required>
-        <label for="uemail"><b>Email</b></label>
+        <br>
+        <label for="email"><b>Email</b></label>
         <input type="email" placeholder="Enter Email" name="email" value="" required>
+        <br>
+        <label for="phone"><b>Phone Number</b></label>
+        <input type="number"placeholder="Enter Phone Number" name="phone" value="" required>
+        <br>
+        <label for="birthday"><b>Birthday</b></label>
+        <input type="date" placeholder="Enter Birthday" name="birthday" value="" required>
+        <br>
+        <label for="skype"><b>Skype</b></label>
+        <input type="text" placeholder="Enter Skype" name="skype" value="" required>
+        <br>
+        <label for="team"><b>Your team</b></label>
+        <select name="team" placeholder="Your team" required>
+            <option value="team1">team1</option>
+            <option value="team2">team2</option>
+            <option value="team3">team3</option>
+            <option value="team4">team4</option>
+        </select>
+        <br>
+        <label for="ADMsince"><b>ADM since</b></label>
+        <input type="date" placeholder="ADM Join Date" name="ADMsince" value="" required>
+        <br>
 
         <button type="submit">Submit</button>
-
+</div>
     </form>
 
 @endsection
