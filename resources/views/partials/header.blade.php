caa7dbabb25f24919d679d3e3f0b1de6b9093159
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-07-04 12:36:42 +0300

    tyops cleanup

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header id="m_header" class="m-grid__item m-header" m-minimize="minimize" m-minimize-mobile="minimize" m-minimize-offset="10" m-minimize-mobile-offset="10" >
+<header id="m_header" class="m-grid__item m-header" m-minimize="minimize" m-minimize-mobile="minimize" m-minimize-offset="10" m-minimize-mobile-offset="10">
     <div class="m-header__top">
         <div class="m-container m-container--fluid m-container--full-height m-page__container">
             <div class="m-stack m-stack--ver m-stack--desktop">
@@ -186,5 +186,6 @@
     </div>
 
     <div class="m-header__bottom">
+
     </div>
 </header>
