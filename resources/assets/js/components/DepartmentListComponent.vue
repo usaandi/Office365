33fad8c3946d654546c01017133dc294619ab598
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-07-31 16:25:15 +0300

    Component to display department

--- resources/assets/js/components/DepartmentListComponent.vue
+++ resources/assets/js/components/DepartmentListComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="list-wrapper ">
+    <div class="list-wrapper">
         <div v-for="department in departments" class="profiles-view align-center">
             <department-list-view v-bind:department="department"></department-list-view>
         </div>
@@ -15,27 +15,46 @@
 
 <style scoped>
     .list-wrapper {
-        width: 33%;
+        width: 100%;
         display: flex;
         flex-wrap: wrap;
         margin: -2rem;
-
-
     }
     .profiles-view {
         border: 1px solid #e1e1e1;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-        flex: 1 0 100%;
+        flex: 1 0 33%;
         margin: 1rem;
     }
     .align-center{
-        justify-content: center;
-        align-items: center;
+
         text-align: center;
     }
-    @media screen and (max-width: 768px) {
-        .list-wrapper {
-            width: 100%;
-        }
-    }
+    /*@media screen and (min-width: 768px) {*/
+        /*.list-wrapper {*/
+            /*flex: 1 0 50%;*/
+
+
+        /*}*/
+    /*}*/
+    /*@media screen and (min-width: 424px) {*/
+        /*.list-wrapper {*/
+            /*width:100%;*/
+            /*display: flex;*/
+            /*flex-wrap: wrap;*/
+
+        /*}*/
+    /*}*/
+    /*@media screen and (min-width: 1039px) {*/
+        /*.profiles-view {*/
+            /*flex: 1 0 100%;*/
+            /*height: 100px;*/
+        /*}*/
+        /*.list-wrapper {*/
+            /*flex: 1 0 50%;*/
+            /*display: flex;*/
+            /*flex-wrap: wrap;*/
+
+        /*}*/
+    /*}*/
 </style>
