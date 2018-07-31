333a2f00c28ac55f6a391b3e287b2cb7f46b3317
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-07-31 16:22:18 +0300

    Controller for department

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -1,18 +1,47 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Department;
+
+
 use Illuminate\Http\Request;
+use App\Department;
 
 class DepartmentController extends Controller
 {
-
-
     public function department(){
 
         $departments = Department::all();
-
         return view('team.departmentView')->with('departments',$departments);
+    }
+    public function view(){
+
+        return view('team.departmentAdd');
+    }
+    public function store(Request $request){
+
+        $request->validate([
+            'departmentName'=>'required|string|min:3|max:255',
+            'departmentAbbr'=> 'required|string|min:3|max:10',
+            'description'=> 'required|string|min:3|max:255',
+        ]);
+        $departmentName=$request->input('departmentName');
+        $departmentAbbr=$request->input('departmentAbbr');
+        $description=$request->input('description');
+
+
+        $department = Department::where('department_name', '=', $departmentName)->first();
+
+        if($department===null){
+
+            Department::create(['department_name' => $departmentName,
+            'department_abbr'=>$departmentAbbr, 'department_info' => $description]);
+
+            return redirect()->back();
+        }
+        else {
+            return response('Duplicate Department entry', 400)
+                ->header('Content-Type', 'application/json');
+        }
 
     }
 }
