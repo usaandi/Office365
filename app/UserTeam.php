18e07b5c1b797120d3c682fbf0249b0fea505252
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-08-01 16:27:45 +0300

    Extends to pivot

--- app/UserTeam.php
+++ app/UserTeam.php
@@ -2,14 +2,6 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class UserTeam extends Pivot
-{
-    protected $table=('users_teams');
-
-    public function user(){
-        return $this->belongsTo('App\User');
-    }
-}
+class UserTeam extends Pivot {}
