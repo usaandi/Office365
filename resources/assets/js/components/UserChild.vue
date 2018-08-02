79337c9cb747a64c866bac57e074009d881ecb9a
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-08-02 15:04:46 +0300

    Displays users children

--- resources/assets/js/components/UserChild.vue
+++ resources/assets/js/components/UserChild.vue
@@ -1,10 +1,40 @@
 <template>
-    $END$
+    <div>
+        <div class="">
+            <div v-for="child in userchildren">
+                <p>{{ child.name }} | {{child.year_born}}</p>
+            </div>
+            + Add new
+        </div>
+
+    </div>
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
-        name: "UserChild"
+        name: "UserHobby",
+        props: ['userid'],
+        data() {
+            return {
+                id: '',
+                userchildren: [],
+                childname: '',
+            }
+        },
+        mounted() {
+            this.id = this.userid;
+            this.fetchData();
+        },
+        methods:{
+            fetchData: function () {
+                axios.get('http://localhost/user/' + this.id + '/child')
+                    .then(response => {
+                        this.userchildren = response.data;
+                    });
+            },
+        }
     }
 </script>
 
