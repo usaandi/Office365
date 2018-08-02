33dec52787ca4fd71d75ce5b3827e72359eae9ed
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-08-02 15:07:47 +0300

    returns Users hobbies data

---
+++ app/Http/Controllers/UserHobbyController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Hobby;
+use App\User;
+
+
+class UserHobbyController extends Controller
+{
+    public function userHobbies(Request $request, $id)
+    {
+        $userid=User::findOrFail($id);
+
+        $data=$userid->hobbies()->get(['hobby_name'])->toArray();
+
+        if (!empty($data)) {
+
+            foreach ($data as $key => $val) {
+
+                if (array_key_exists('pivot', $data[$key])) {
+                    unset($data[$key]['pivot']);
+                }
+
+            }
+
+        }
+
+        return $data;
+    }
+}
