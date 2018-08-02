200e17247469bcfb7da34b3d4708005bdc5bb8dc
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-08-02 15:08:06 +0300

    Vue view to add more hobbies

---
+++ resources/assets/js/components/HobbyAdd.vue
@@ -0,0 +1,14 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        props: ['userhobbies'],
+        name: "HobbyAdd"
+    }
+</script>
+
+<style scoped>
+
+</style>
