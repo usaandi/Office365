8a575653eb025c1922ed80938c8e4519a04ed21a
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-08-07 08:24:47 +0300

    whitespace

--- resources/views/partials/htmlhead.blade.php
+++ resources/views/partials/htmlhead.blade.php
@@ -22,3 +22,4 @@
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
