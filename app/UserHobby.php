706c60a326948c9ec6c87134ed713be539658a42
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-08-07 08:19:14 +0300

    Hobby belongs to children

--- app/UserHobby.php
+++ app/UserHobby.php
@@ -6,4 +6,8 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class UserHobby extends Pivot {
 
+    protected $table = 'users_hobbies';
+
+    protected $fillable = ['user_id', 'hobby_id'];
+
 }
