81dd502c0a136d4a04f89c3d9c79cc24f1203757
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-08-07 08:17:50 +0300

    Belongs to app\team

--- app/TeamModerator.php
+++ app/TeamModerator.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class TeamModerator extends Pivot
 {
-    protected $table=('teams_moderators');
+    protected $table= 'teams_moderators';
 
     public function moderator(){
         return $this->belongsTo('App\Team');
