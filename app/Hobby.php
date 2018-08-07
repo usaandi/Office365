bb1248ae3df7c64fe803a73bc4fb726a2d55ff87
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-08-07 08:13:04 +0300

    made table fillable

--- app/Hobby.php
+++ app/Hobby.php
@@ -8,6 +8,8 @@ class Hobby extends Model
 {
     protected $table = 'hobbies';
 
+    protected $fillable = ['hobby_name'];
+
     public function users() {
         return $this->belongsToMany('App\User', 'users_hobbies')
             ->using('App\UserHobby');
--- resources/assets/js/components/HobbyAddNew.vue
+++ resources/assets/js/components/HobbyAddNew.vue
@@ -1,10 +1,10 @@
 <template>
-    
+    $END$
 </template>
 
 <script>
     export default {
-        name: "UserHobbyView"
+        name: "HobbyAddNew"
     }
 </script>
 
