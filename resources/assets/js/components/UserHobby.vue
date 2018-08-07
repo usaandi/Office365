7fdcc14c445b6eecbffbaffb03b71322ef1bba52
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-08-07 08:24:02 +0300

    Displays user hobbies and can edit those

--- resources/assets/js/components/UserHobby.vue
+++ resources/assets/js/components/UserHobby.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        <div class="user-list-wrapper">
-            <div v-for="hobby in userhobbies">
-               <p>#{{ hobby.hobby_name }}</p>
+        <div>
+            <div class="fc-button-group">
+                    <button @click="edit=!edit" >Edit</button>
             </div>
-            <hobby-add></hobby-add>
+                <div class="" v-for="hobby in userhobbies">
+                    <p class="d-inline-block">#{{ hobby.hobby_name }}</p>
+                    <button v-show="edit" class="" @click="deleteRow(hobby.id)">X</button>
+                </div>
+            <button v-show="edit" @click="upload()" >+Add new</button>
+            <input v-show="edit" v-model="hobbyname">
         </div>
-
     </div>
 </template>
 
@@ -15,25 +19,60 @@
 
     export default {
         name: "UserHobby",
-        props: ['userid'],
+        props: ['userid','canedit'],
         data() {
             return {
                 id: '',
                 userhobbies: [],
                 hobbyname: '',
+                edit: 'false'
             }
         },
         mounted() {
             this.id = this.userid;
             this.fetchData();
+            console.log(this.canedit);
+            this.edit = this.canedit;
         },
         methods:{
-            fetchData: function () {
-              axios.get('http://localhost/user/' + this.id + '/hobby')
-                  .then(response => {
-                      this.userhobbies = response.data;
-                  });
+
+            deleteRow: function(userHobbyId){
+                console.log(userHobbyId);
+                let vm =this;
+                axios.delete('http://localhost/user/'+ this.id +'/delete/hobby', {params: {userhobbyid: userHobbyId}})
+                    .then(function (response) {
+                        let index = vm.userhobbies.findIndex(function(obj){
+                            return obj.id === userHobbyId;
+                        });
+                        if (index !== -1) {
+                            vm.userhobbies.splice(index, 1);
+                        }
+                    } )
+
+
             },
+
+            fetchData: function () {
+                axios.get('http://localhost/user/' + this.id + '/hobby')
+                    .then(response => {
+                        this.userhobbies = response.data;
+                    });
+            },
+
+            upload: function () {
+                let vm = this;
+                axios.post('http://localhost/user/' + this.id + '/update/hobby',{data: this.hobbyname})
+                    .then(response => {
+                        vm.userhobbies.push({
+                            id: response.data.user_hobby_id,
+                            hobby_name: response.data.hobby_name
+                        });
+                        vm.edit = false;
+                    }).catch(error => {
+                        console.log(error.response.data);
+                    });
+            },
+
         }
     }
 </script>
