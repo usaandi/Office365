6b34b80020f81d3da57c0b87feb392f946e26d96
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-08-07 08:22:14 +0300

    Might remove this later

--- resources/assets/js/components/HobbyAddNew.vue
+++ resources/assets/js/components/HobbyAddNew.vue
@@ -1,10 +1,35 @@
 <template>
-    $END$
+    <div>
+        <div v-show="!add">
+            <input ref="edit"  >
+        </div>
+        <button  v-on:click="add=!add" >Edit</button>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "HobbyAddNew"
+        name: "HobbyAddNew",
+        data: function () {
+            return{
+                add:true,
+            }
+        
+        },
+        methods: {
+
+
+
+            showInput: function () {
+                console.log(add.toString());
+                if (this.add) {
+                    this.add = false;
+                }
+                else {
+                    this.add = true;
+                }
+            }
+        }
     }
 </script>
 
