f00915c3c8ad0560e87bb668c107364c2ad6cd92
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-08-29 14:11:58 +0300

    Model for Created DB tabels

---
+++ app/UserCareerRoleMilestone.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserCareerRoleMilestone extends Model
+{
+     protected $table = 'users_career_roles_milestones';
+
+     protected $fillable=[
+         'milestone_id',
+         'user_id',
+         'assigned_id',
+         'task',
+         'reminder',
+         'completed'
+     ];
+
+     public function user(){
+         return $this->hasOne(User::class);
+     }
+     public function careerRoleMilestone(){
+         return $this->hasOne(CareerRoleMilestone::class);
+     }
+
+}
