858f4e06ce894971888061120244ffe50371f789
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-09-03 16:22:35 +0300

    Auth guard on routes

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@ use App\User;
 |
 */
 
-route::get('/', function (){
 
-});
 
 //Route::get('/register', 'HomeController@index');
 //Route::get('/signin', 'AuthController@signin');
@@ -21,11 +19,13 @@ Route::get('/', 'AuthController@signin');
 Route::get('/authorize', 'AuthController@gettoken');
 
 
-Route::get('/upload', 'ImageController@show');
-Route::post('/upload/{id}', 'ImageController@upload')->name('upload');
 
 
-Auth::routes();
+
+
+Route::group(['middleware'=> 'auth'], function (){
+    Route::get('/upload', 'ImageController@show');
+    Route::post('/upload/{id}', 'ImageController@upload')->name('upload');
 Route::get('/user/{id}/update', 'UserController@showedit');
 Route::post('/user/{id}/update', 'UserController@update')->name('update');
 Route::get('/user/{id}/hobby', 'UserHobbyController@userHobbies');
@@ -40,7 +40,6 @@ Route::post('/user/{id}/update/hobby', 'HobbyController@updateHobby')->name('upd
 Route::post('/user/{id}/update/info', 'UserController@updateInfo')->name('updateInfo');
 Route::post('/user/{id}/update/child', 'UserChildController@updateChild')->name('updateChild');
 Route::delete('user/{id}/delete/child', 'UserChildController@deleteUserChild')->name('deleteUserChild');
-
 Route::post('/user/{id}/department', 'TeamsController@updateDepartment')->name('updateDepartment');
 Route::post('/user/{id}/team', 'TeamsController@updateUserTeam')->name('updateUserTeam');
 Route::get('/user/{id}/career', 'PersonalDevelopmentController@userDevelopment')->name('career');
@@ -55,10 +54,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/usersDepartment','HomeController@userNoDepartment')->name('users');
 Route::get('department/add','DepartmentController@view')->name('departmentadd');
 Route::post('department/add','DepartmentController@store')->name('store');
-
-
 Route::get('department/{id}', 'DepartmentController@show')->name('team');
 Route::get('department/{id}/user', 'DepartmentController@userDepartment')->name('user');
 Route::get('departmentInfo', 'DepartmentController@departmentInfo')->name('departmentInfo');
 Route::get('teamInfo', 'TeamsController@teamInfo')->name('teamInfo');
 Route::get('department', 'DepartmentController@department')->name('department');
+});
