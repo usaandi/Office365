b9c8db63d7ec366c82a70534bd3a2b9a8b9ef961
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-09-03 10:46:40 +0300

    Renamed routes to be more logical

--- routes/web.php
+++ routes/web.php
@@ -43,17 +43,19 @@ Route::delete('user/{id}/delete/child', 'UserChildController@deleteUserChild')->
 
 Route::post('/user/{id}/department', 'TeamsController@updateDepartment')->name('updateDepartment');
 Route::post('/user/{id}/team', 'TeamsController@updateUserTeam')->name('updateUserTeam');
-Route::get('/user/{id}/development', 'PersonalDevelopmentController@userDevelopment')->name('development');
-Route::get('/career/add', 'CareerController@show')->name('career');
+Route::get('/user/{id}/career', 'PersonalDevelopmentController@userDevelopment')->name('career');
+Route::get('/career/add', 'CareerController@show')->name('addCareer');
 Route::post('/career/add', 'CareerController@create')->name('addCareer');
 Route::delete('user/{id}/delete/hobby', 'HobbyController@deleteUserHobby')->name('deleteUserHobby');
 Route::get('/user/{id}', 'UserController@show')->name('profile');
+Route::get('user/{id}/career/info','CareerController@returnUserData')->name('careerInfo');
 Route::get('/admin/users/add', 'AddController@add')->name('useradd');
 Route::post('/admin/users/add', 'AddController@store')->name('store');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/usersDepartment','HomeController@userNoDepartment')->name('users');
 Route::get('department/add','DepartmentController@view')->name('departmentadd');
 Route::post('department/add','DepartmentController@store')->name('store');
+
 
 Route::get('department/{id}', 'DepartmentController@show')->name('team');
 Route::get('department/{id}/user', 'DepartmentController@userDepartment')->name('user');
