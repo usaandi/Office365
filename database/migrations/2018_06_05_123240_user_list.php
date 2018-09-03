91c3eb7ab1b477bed7e0673aa6d9ce2459f795d0
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-09-03 14:08:29 +0300

    Fixed migration file

--- database/migrations/2018_06_05_123240_user_list.php
+++ database/migrations/2018_06_05_123240_user_list.php
@@ -17,7 +17,7 @@ class UserList extends Migration
 
             $table->increments('id');
             $table->string('email')->unique();
-            $table->integer('phoneN');
+            $table->integer('phone');
             $table->string('name');
             $table->date('birthday');
             $table->string('skype');
