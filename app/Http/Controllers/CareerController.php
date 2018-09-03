5fc8d31c2b71de7fd925b35d2821c2497bf6642c
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-09-03 10:43:39 +0300

    Function to return this userdata for Career View

--- app/Http/Controllers/CareerController.php
+++ app/Http/Controllers/CareerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\CareerRole;
 use Validator;
+use App\User;
 
 class CareerController extends Controller
 {
@@ -18,7 +19,6 @@ class CareerController extends Controller
         try {
 
             $data=json_decode(key($request->all()), true);
-
             $rules = [
                 'title' => 'required|string|max:30',
                 'description' => 'required|'
@@ -59,5 +59,62 @@ class CareerController extends Controller
 
         }
 
+    }
+
+    public function returnUserData( $id){
+
+
+        $userCareerRole= array();
+        $userCareerRoleMilestone= array();
+
+        $user=User::find($id);
+
+        $userscr=$user->userCareerRole()
+            ->get(['id','career_role_id','user_id','title','description']);
+
+        $userscrm=$user->userCareerRoleMilestones()
+            ->get(['id','user_id','milestone_id','assigned_id'
+                ,'task','reminder','completed']);
+
+        if(!empty($userscr)){
+
+            foreach ($userscr as $i =>$usercr){
+
+                $userCareerRoleId=$usercr->id;
+                $careerRoleId=$usercr->career_role_id;
+                $userId=$usercr->user_id;
+                $title=$usercr->title;
+                $description=$usercr->description;
+
+                $userCareerRole[$i]['id']=$userCareerRoleId;
+                $userCareerRole[$i]['career_role_id']=$careerRoleId;
+                $userCareerRole[$i]['user_id']=$userId;
+                $userCareerRole[$i]['title']=$title;
+                $userCareerRole[$i]['description']=$description;
+            }
+            unset($usercr);
+        }
+        if(!empty($userscrm)){
+            foreach ($userscrm as $i =>$usercrm){
+
+                $careerRoleMilestoneId=$usercrm->id;
+                $careerRoleMilestoneUserId=$usercrm->user_id;
+                $milestoneId=$usercrm->milestone_id;
+                $assignerId=$usercrm->assigned_id;
+                $taskName=$usercrm->task;
+                $reminder=$usercrm->reminder;
+                $completed=$usercrm->completed;
+
+                $userCareerRoleMilestone[$i]['career_role_milestone_id']=$careerRoleMilestoneId;
+                $userCareerRoleMilestone[$i]['user_id']=$careerRoleMilestoneUserId;
+                $userCareerRoleMilestone[$i]['milestone_id']=$milestoneId;
+                $userCareerRoleMilestone[$i]['assigned_id']=$assignerId;
+                $userCareerRoleMilestone[$i]['task']=$taskName;
+                $userCareerRoleMilestone[$i]['reminder']=$reminder;
+                $userCareerRoleMilestone[$i]['completed']=$completed;
+            }
+            unset($usercrm);
+        }
+        return [$userCareerRole,$userCareerRoleMilestone];
     }
 }
