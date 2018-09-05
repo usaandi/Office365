2bb3c99c71036a3757642c60f9b332ca8c877df7
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-09-05 15:14:08 +0300

    main Career view

--- resources/assets/js/components/career/CareerComponent.vue
+++ resources/assets/js/components/career/CareerComponent.vue
@@ -28,83 +28,9 @@
                                         </p>
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col-sm-3 col-md-3 col-lg-2 col-xs-12">
-                                        <h4 class="profile-timeline__title">MILESTONES</h4>
-                                    </div>
-                                    <div class="col-sm-9 col-md-9 col-lg-10 col-xs-12">
-                                        <div class="profile-timeline__add">
-                                            <a href="#" class="btn btn-success m-btn m-btn--icon m-btn--pill">
-                              <span>
-                                <i class="la la-plus"></i>
-                                <span>New</span>
-                              </span>
-                                            </a>
-                                            <div class="profile-timeline__form">
-                                                <form class="m-form">
-                                                    <div class="m-portlet__body">
-                                                        <div class="form-group m-form__group row">
-                                                            <label for="example-text-input" class="col-3 col-form-label">Task</label>
-                                                            <div class="col-9">
-                                                                <input type="text" class="form-control m-input" placeholder="Task">
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group m-form__group row">
-                                                            <label for="example-text-input" class="col-3 col-form-label">Assign</label>
-                                                            <div class="col-9">
-                                                                <select class="form-control m-input" id="exampleSelect1">
-                                                                    <option>me</option>
-                                                                </select>
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group m-form__group row">
-                                                            <label for="example-text-input" class="col-3 col-form-label">Set reminder</label>
-                                                            <div class="col-9">
-                                                                <input type="date" class="form-control m-input" placeholder="Task">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="m-portlet__foot m-portlet__foot--fit">
-                                                        <div class="m-form__actions">
-                                                            <div class="row">
-                                                                <div class="col-sm-3 col-xs-12">
-                                                                </div>
-                                                                <div class="col-sm-9 col-xs-12">
-                                                                    <div class="profile-timeline__action"><button type="button" class="btn m-btn--pill btn-outline-success m-btn m-btn--custom">Cancel</button>
-                                                                        <button type="button" class="btn m-btn--pill btn-success m-btn m-btn--custom">Save</button></div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                        <div class="profile-timeline__milestone">
-                                            <div class="m-form__group form-group">
-                                                <div class="m-checkbox-list">
-                                                    <label class="m-checkbox m-checkbox--air m-checkbox--state-success">
-                                                        <input type="checkbox"> <div class="profile-timeline__milestones--label">CSS Masterclass Course</div>
-                                                        <span></span>
-                                                        <div class="profile-timeline__milestones--name">
-                                                            <div class="m-list-pics m-list-pics--sm">
-                                                                <a href="#"><img src="" title=""></a>
-                                                                Tiina-Maria Liblikas
-                                                            </div>
-                                                        </div>
-                                                        <div class="profile-timeline__milestones--date m--font-danger">
-                                                            12.12.18
-                                                        </div>
-                                                        <div class="profile-timeline__milestones--action">
-                                                            <a href="#" class="btn btn-info m-btn m-btn--icon btn-sm m-btn--icon-only  m-btn--pill">
-                                                                <i class="la la-pencil-square"></i>
-                                                            </a>
-                                                        </div>
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <user-career-milestone></user-career-milestone>
+
+
                                 <div class="row">
                                     <div class="col-sm-3 col-md-3 col-lg-2 col-xs-12">
                                         <div class="profile-timeline__notes">
