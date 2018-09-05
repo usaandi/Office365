1f948b727e1e31ae41a7dc81a8180c81cd59a31f
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-09-05 14:00:32 +0300

    removed unused select2 lib

--- resources/views/partials/footerscripts.blade.php
+++ resources/views/partials/footerscripts.blade.php
@@ -13,7 +13,6 @@
     var authUser = {!! auth()->user() !!};
     var authUserRole = {!! auth()->user()->roles()->first() !!};
 </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 <script src="https://unpkg.com/vue@latest"></script>
 <script src="https://unpkg.com/vue-select@latest"></script>
 
