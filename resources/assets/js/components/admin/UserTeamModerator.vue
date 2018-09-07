f814b6aa4ef215e6f1e49d52bb7a427f293eccc9
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-09-07 10:14:32 +0300

    view to add moderators to teams

--- resources/assets/js/components/admin/UserTeamModerator.vue
+++ resources/assets/js/components/admin/UserTeamModerator.vue
@@ -1,10 +1,72 @@
 <template>
-    $END$
+<div>
+        <div class="card-body">
+            <div class="form-group">
+                <h1>Team Moderators</h1>
+                <label for="Team name"><b>Team Name</b></label>
+                <select id="team" name="team"  @change="onChangeTeam">
+                    <option
+                            v-for="team in teamsList"
+                            :value="team.id"
+                    >{{team.team_name}}</option>
+                </select>
+                <br>
+                <label for="Team name"><b>Moderator name</b></label>
+                <select id="user" name="user" @change="onChangeUser" >
+                    <option
+                            v-for="user in usersList"
+                            :value="user.id"
+                    >{{user.name}}</option>
+                </select>
+                <br>
+                <button type="submit" @click="submit()">Submit</button>
+            </div>
+        </div>
+</div>
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
-        name: "TeamModerator"
+        props:['users','teams'],
+        name: "TeamModerator",
+        data(){
+            return{
+                usersList:[],
+                teamsList:[],
+                userId:'',
+                teamId:'',
+            }
+        },
+        mounted(){
+          this.usersList=this.users;
+          this.teamsList=this.teams;
+        },
+        methods:{
+            onChangeUser(event){
+              this.userId=event.target.value;
+            },
+            onChangeTeam(event){
+                this.teamId=event.target.value;
+            },
+            submit(){
+                let data= JSON.stringify({
+                    userId: this.userId,
+                    teamId: this.teamId,
+
+                });
+                console.log(this.teamId +''+ this.userId);
+                axios.post('/admin/team/moderator/add',data)
+                    .then(response => {
+
+
+                    }).catch(error => {
+                    console.log(error.response.data);
+                });
+            }
+
+        }
     }
 </script>
 
