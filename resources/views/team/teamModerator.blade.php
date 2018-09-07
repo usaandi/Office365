a071c5e502a893615ef37cf70700bb358415550d
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-09-07 10:17:19 +0300

    view to add moderators to teams

---
+++ resources/views/team/teamModerator.blade.php
@@ -0,0 +1,44 @@
+
+@extends('layouts.master')
+
+@section('content-header')
+
+    <div class="d-flex align-items-center">
+        <div class="mr-auto">
+            <h3 class="m-subheader__title ">
+                Hello
+            </h3>
+        </div>
+        <div>
+            <span class="m-subheader__daterange" id="m_dashboard_daterangepicker">
+                <span class="m-subheader__daterange-label">
+                    <span class="m-subheader__daterange-title"></span>
+                    <span class="m-subheader__daterange-date m--font-brand"></span>
+                </span>
+                <a href="#" class="btn btn-sm btn-brand m-btn m-btn--icon m-btn--icon-only m-btn--custom m-btn--pill">
+                    <i class="la la-angle-down"></i>
+                </a>
+            </span>
+        </div>
+        @include('partials.footerscripts')
+    </div>
+@endsection
+
+@section('content-body')
+    <style>
+         select {
+            width:200px;
+            border:1px solid #babacc;
+            box-sizing:border-box;
+
+        }
+
+        label {
+            width: 100px;
+        }
+    </style>
+    <div id="app">
+        <user-team-moderator :users='@json($users)' :teams='@json($teams)' ></user-team-moderator>
+    </div>
+
+@endsection
