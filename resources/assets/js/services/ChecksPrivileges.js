0384c738cbf5e3d4b341386f9dd1a44140c30278
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-09-10 09:38:37 +0300

    Check if user is moderator or admin

--- resources/assets/js/services/ChecksPrivileges.js
+++ resources/assets/js/services/ChecksPrivileges.js
@@ -3,8 +3,25 @@ const ChecksPrivileges = {
     install(Vue, options) {
 
         Vue.$isAdmin = () => {
-            return authUserRole.name === 'Admin';
+            return isAdmin;
+        };
+
+        Vue.$moderatesTeams = () => {
+            return userModeratedTeams;
+        };
+
+        Vue.$canModerateTeam = (teamId) => {
+
+            for (let i = 0; i < userModeratedTeams.length; i++) {
+                if (userModeratedTeams[i].team_id === teamId) {
+                    return true;
+                }
+            }
+
+            return false;
+
         }
+
     }
 
 };
