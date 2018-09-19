a3f280b4015258ca126a8e2f2e35ff129059c71a
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-09-19 09:58:32 +0300

    main view of user career

--- resources/assets/js/components/career/CareerComponent.vue
+++ resources/assets/js/components/career/CareerComponent.vue
@@ -1,67 +1,71 @@
 <template>
-    <div>
-        <div class="profile-timeline">
-            <div class="m-timeline-2">
-                <div class="m-timeline-2__items">
-                    <div class="m-timeline-2__item">
-                        <span class="m-timeline-2__item-time">2020 Jan</span>
-                        <div class="m-timeline-2__item-cricle">
-                            <i class="fa fa-genderless m--font-info"></i>
-                        </div>
-                        <div class="m-timeline-2__item-text  m--padding-top-5">
-                            <div class="profile-timeline__content">
-                                <div class="row">
-                                    <div class="col-sm-3 col-md-3 col-lg-2 col-xs-12">
-                                        <h4 class="profile-timeline__title">FINAL BOSS</h4>
-                                    </div>
-                                    <div class="col-sm-9 col-md-9 col-lg-10 col-xs-12">
-                                        <p class="profile-timeline__text">
-                                            Coordinate with designers and developers to develop various new products and features along with various interactive designs
-                                            within required timeframe. Develop new business initiatives in coordination with UE team and
-                                            design various methodologies and practices for innovative interactive technologies and perform
-                                            quality assurance and browser computability tests. Design style sheets for website and assist
-                                            application programmers in effective implementation and ensure all work according to scripting
-                                            code with help of various technologies. Collaborate with designers and application developer
-                                            to develop various customized websites. Prepare all production documents on Photoshop and translate
-                                            same into HTML templates with help of developers and designers and facilitate to introduce new
-                                            features into same.
-                                        </p>
-                                    </div>
-                                </div>
-                                <user-career-milestone></user-career-milestone>
-                                <div class="row">
-                                    <div class="col-sm-3 col-md-3 col-lg-2 col-xs-12">
-                                        <div class="profile-timeline__notes">
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-9 col-md-9 col-lg-10 col-xs-12">
-                                        <div class="profile-timeline__action">
-                                            <button type="button" class="btn m-btn--pill btn-outline-success m-btn m-btn--custom">Edit</button>
-                                            <button type="button" class="btn m-btn--pill btn-success m-btn m-btn--custom">Apply as current</button>
-                                        </div>
-                                    </div>
-                                </div>
+<div>
+    <div class="d-flex align-items-center">
 
-                            </div>
-                        </div>
+        <div class="mr-auto">
+            <div class="m-subheader ">
+                <div class="d-flex align-items-center">
+                    <div>
+                        <h3 class="m-subheader__title ">
+                            Personal development
+                        </h3>
                     </div>
-
+                    <div class="m-subheader__action">
+                        <a href="" class="btn btn-success m-btn m-btn--icon m-btn--pill">
+                            <span>
+                                <i class="la la-plus"></i>
+                                <span>New</span>
+                            </span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="profile-timeline">
+        <div class="m-timeline-2">
+            <div class="m-timeline-2__items" >
+            <career-role
+                    :authUserId="AuthUserId" v-for="userInfo in userdatas" :userdata="userInfo" :key="userInfo.id">
+            </career-role>
+            </div>
+        </div>
+    </div>
+
+</div>
 </template>
 
 <script>
+    import axios from 'axios';
 
     export default {
+        props:['currentUserId'],
         name: "CareerModal",
         data(){
             return{
                 isHidden:false,
-
+                AuthUserId:'',
+                selectedUserId:'',
+                userdatas:null,
             }
         },
+
+
+        mounted(){
+            this.AuthUserId = authUser.id;
+            this.fetchData();
+        },
+        methods:{
+
+
+            fetchData: function () {
+                axios.get('/user/' + this.currentUserId + '/career/info')
+                    .then(response => {
+                        this.userdatas = response.data;
+                    });
+
+            }
+        }
 
     }
 </script>
