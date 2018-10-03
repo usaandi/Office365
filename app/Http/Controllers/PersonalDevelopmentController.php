d609aea7e6a9eb8970541d417c0b45fad826c02a
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-10-03 15:28:40 +0300

    deleted empty space

--- app/Http/Controllers/PersonalDevelopmentController.php
+++ app/Http/Controllers/PersonalDevelopmentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Team;
+use App\Department;
 
 class PersonalDevelopmentController extends Controller
 {
@@ -12,5 +14,27 @@ class PersonalDevelopmentController extends Controller
         $user = User::findOrFail($id)->id;
 
         return view('user.usercareer', compact('user'));
+    }
+    public function userInfo($id)
+    {
+        $authUserId = \Auth::user()->id;
+
+        $userModel = User::findorFail($id);
+        $user = $userModel->toArray();
+
+        $userDepartment = $userModel->department()->get(['department_id'])->first();
+        $userTeam =$userModel->team()->get(['team_id'])->first();
+
+        if ($userDepartment !== NULL) {
+            $department = Department::find($userDepartment->department_id);
+            $user['department'] = $department->department_name;
+            $user['department_id'] = $department->id;
+        }
+        if($userTeam !== NULL){
+            $team = Team::find($userTeam->team_id);
+            $user['team'] = $team->team_name;
+            $user['team_id'] = $team->id;
+        }
+        return $user;
     }
 }
