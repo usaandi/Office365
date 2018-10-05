a85114582a291848c05dc526f168bd944fd85cef
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-10-05 12:07:05 +0300

    header updates

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -65,16 +65,17 @@
                     <div id="m_header_topbar" class="m-topbar  m-stack m-stack--ver m-stack--general">
                         <div class="m-stack__item m-topbar__nav-wrapper">
                             <ul class="m-topbar__nav m-nav m-nav--inline">
-                                <li class="m-nav__item m-topbar__user-profile  m-dropdown m-dropdown--medium m-dropdown--arrow  m-dropdown--align-right m-dropdown--mobile-full-width m-dropdown--skin-light" m-dropdown-toggle="click">
-                                    <a href="#" class="m-nav__link m-dropdown__toggle">
-													<span class="m-topbar__userpic">
-														<img src="{{asset('assets/app/media/img/users/user4.jpg')}}" class="m--img-rounded m--marginless m--img-centered" alt=""/>
+                                <li class="m-nav__item m-topbar__user-profile  m-dropdown m-dropdown--medium m-dropdown--arrow  m-dropdown--align-right m-dropdown--mobile-full-width m-dropdown--skin-light"
+                                    m-dropdown-toggle="click">
+                                    <a href="" class="m-nav__link m-dropdown__toggle">
+                      <span class="m-topbar__userpic">
+														<img src="" class="m--img-rounded m--marginless m--img-centered" alt=""/>
 													</span>
                                         <span class="m-nav__link-icon m-topbar__usericon  m--hide">
 														<span class="m-nav__link-icon-wrapper">
 															<i class="flaticon-user-ok"></i>
 														</span>
-													</span>
+                      </span>
                                         <span class="m-topbar__username m--hide">
 														Nick
 													</span>
@@ -85,10 +86,10 @@
                                             <div class="m-dropdown__header m--align-center">
                                                 <div class="m-card-user m-card-user--skin-light">
                                                     <div class="m-card-user__pic">
-                                                        <img src="{{asset('assets/app/media/img/users/user4.jpg')}}" class="m--img-rounded m--marginless" alt=""/>
+                                                        <img src="" class="m--img-rounded m--marginless" alt="" />
                                                     </div>
                                                     <div class="m-card-user__details">
-																	<span class="m-card-user__name m--font-weight-500">
+                              <span class="m-card-user__name m--font-weight-500">
 																		Mark Andre
 																	</span>
                                                         <a href="" class="m-card-user__email m--font-weight-300 m-link">
@@ -101,7 +102,7 @@
                                                 <div class="m-dropdown__content">
                                                     <ul class="m-nav m-nav--skin-light">
                                                         <li class="m-nav__section m--hide">
-																		<span class="m-nav__section-text">
+                                <span class="m-nav__section-text">
 																			Section
 																		</span>
                                                         </li>
@@ -113,25 +114,25 @@
 																					<span class="m-nav__link-text">
 																						My Profile
 																					</span>
-																					<span class="m-nav__link-badge">
+                                  <span class="m-nav__link-badge">
 																						<span class="m-badge m-badge--success">
 																							2
 																						</span>
-																					</span>
-																				</span>
-																			</span>
+                                  </span>
+                                  </span>
+                                  </span>
                                                             </a>
                                                         </li>
                                                         <li class="m-nav__item">
                                                             <a href="{{route('career',['id'=>auth()->user()])}}" class="m-nav__link">
                                                                 <i class="m-nav__link-icon flaticon-share"></i>
                                                                 <span class="m-nav__link-text">
-																				Personal Development
+																				Personal career
 																			</span>
                                                             </a>
                                                         </li>
                                                         <li class="m-nav__item">
-                                                            <a href="profile.html" class="m-nav__link">
+                                                            <a href="" class="m-nav__link">
                                                                 <i class="m-nav__link-icon flaticon-chat-1"></i>
                                                                 <span class="m-nav__link-text">
 																				Messages
@@ -167,14 +168,74 @@
                                         </div>
                                     </div>
                                 </li>
-                                <li id="m_quick_sidebar_toggle" class="m-nav__item">
+                                <li id="m_quick_sidebar_toggle" class="m-nav__item m-dropdown m-dropdown--medium m-dropdown--arrow m-dropdown--align-right m-dropdown--mobile-full-width m-dropdown--skin-light"
+                                    m-dropdown-toggle="click">
                                     <a href="#" class="m-nav__link m-dropdown__toggle">
-													<span class="m-nav__link-icon m-nav__link-icon-alt">
+                        <span class="m-nav__link-icon m-nav__link-icon-alt">
 														<span class="m-nav__link-icon-wrapper">
 															<i class="flaticon-grid-menu"></i>
 														</span>
-													</span>
+                        </span>
                                     </a>
+                                    <div class="m-dropdown__wrapper">
+                                        <span class="m-dropdown__arrow m-dropdown__arrow--right m-dropdown__arrow--adjust"></span>
+                                        <div class="m-dropdown__inner">
+                                            <div class="m-dropdown__body">
+                                                <div class="m-dropdown__content">
+                                                    <ul class="m-nav m-nav--icons">
+                                                        <li class="m-nav__item">
+                                                            <a href="{{route('career',['id'=>auth()->user()])}}" class="m-nav__link">
+                                                                <i class="m-nav__link-icon la la-dashboard"></i>
+                                                                <span class="m-nav__link-text">
+																				Statistics
+																			</span>
+                                                            </a>
+                                                        </li>
+                                                        <li class="m-nav__item">
+                                                            <a href="profile.html" class="m-nav__link">
+                                                                <i class="m-nav__link-icon la la-users"></i>
+                                                                <span class="m-nav__link-text">
+																				Team builder
+																			</span>
+                                                            </a>
+                                                        </li>
+                                                        <li class="m-nav__item">
+                                                            <a href="profile.html" class="m-nav__link">
+                                                                <i class="m-nav__link-icon la la-tasks"></i>
+                                                                <span class="m-nav__link-text">
+																				Strengths
+																			</span>
+                                                            </a>
+                                                        </li>
+                                                        <li class="m-nav__item">
+                                                            <a href="profile.html" class="m-nav__link">
+                                                                <i class="m-nav__link-icon la la-map"></i>
+                                                                <span class="m-nav__link-text">
+																				Lorem
+																			</span>
+                                                            </a>
+                                                        </li>
+                                                        <li class="m-nav__item">
+                                                            <a href="profile.html" class="m-nav__link">
+                                                                <i class="m-nav__link-icon la la-map"></i>
+                                                                <span class="m-nav__link-text">
+																				Lorem
+																			</span>
+                                                            </a>
+                                                        </li>
+                                                        <li class="m-nav__item">
+                                                            <a href="profile.html" class="m-nav__link">
+                                                                <i class="m-nav__link-icon la la-map"></i>
+                                                                <span class="m-nav__link-text">
+																				Lorem
+																			</span>
+                                                            </a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </li>
                             </ul>
                         </div>
