0a7884e8a0c840508e1e461cb7350153943a3e65
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-10-16 10:17:22 +0300

    routes cleanup so i can more easily read them

--- routes/web.php
+++ routes/web.php
@@ -28,12 +28,17 @@ Route::group(['middleware'=> 'auth'], function (){
 Route::get('/user/career/info/{id}', 'PersonalDevelopmentController@userInfo');
 Route::get('/career/roles','CareerController@careerRoleMilestonesData');
 Route::get('/upload', 'ImageController@show');
-Route::post('/upload/{id}', 'ImageController@upload')->name('upload');
 Route::get('/user/{id}/update', 'UserController@showedit');
+
+Route::post('/upload/{id}', 'ImageController@upload')->name('upload');
+
+
 Route::post('/user/{id}/update', 'UserController@update')->name('update');
+
 Route::get('/user/{id}/hobby', 'UserHobbyController@userHobbies');
 Route::get('/user/{id}/child', 'UserChildController@userChild');
 Route::get('/hobbies', 'HobbyController@hobby');
+
 Route::post('/user/{id}/update/team', 'UserController@updateTeam')->name('updateTeam');
 Route::post('/user/{id}/update/phone', 'UserController@updatePhone')->name('updatePhone');
 Route::post('/user/{id}/update/email', 'UserController@updateEmail')->name('updateEmail');
@@ -44,8 +49,10 @@ Route::post('/user/{id}/update/child', 'UserChildController@updateChild')->name(
 Route::delete('user/{id}/delete/child', 'UserChildController@deleteUserChild')->name('deleteUserChild');
 Route::post('/user/{id}/department', 'TeamsController@updateDepartment')->name('updateDepartment');
 Route::post('/user/{id}/team', 'TeamsController@updateUserTeam')->name('updateUserTeam');
+
 Route::get('/user/{id}/career', 'PersonalDevelopmentController@userDevelopment')->name('career');
 Route::get('/career/add', 'CareerController@show')->name('addCareer');
+
 Route::post('/career/add', 'CareerController@create')->name('addCareer');
 Route::post('/user/{id}/career/milestone/create', 'CareerController@createMilestone')->name('createMilestone');
 Route::post('/user/{id}/career/milestone/update', 'CareerController@updateMilestone')->name('updateMilestone');
@@ -54,21 +61,29 @@ Route::post('/user/{id}/career/update', 'CareerController@updateCareer')->name('
 Route::post('/user/{id}/career/role/create', 'CareerController@createCareer')->name('createCareer');
 Route::post('/user/{id}/career/role/select', 'CareerController@selectCareer')->name('selectCareer');
 Route::post('/user/{id}/career/role/save', 'CareerController@saveCareer')->name('saveCareer');
+
 Route::delete('user/{id}/delete/hobby', 'HobbyController@deleteUserHobby')->name('deleteUserHobby');
 Route::get('/user/{id}', 'UserController@show')->name('profile');
 Route::get('user/{id}/career/info','CareerController@returnUserData')->name('careerInfo');
 Route::get('/admin/users/add', 'AddController@add')->name('useradd');
 Route::post('/admin/users/add', 'AddController@store')->name('store');
+
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/usersDepartment','HomeController@userNoDepartment')->name('users');
 Route::get('department/add','DepartmentController@view')->name('departmentadd');
+
 Route::post('department/add','DepartmentController@store')->name('store');
+
 Route::get('department/{id}', 'DepartmentController@show')->name('team');
 Route::get('department/{id}/user', 'DepartmentController@userDepartment')->name('user');
 Route::get('departmentInfo', 'DepartmentController@departmentInfo')->name('departmentInfo');
 Route::get('teamInfo', 'TeamsController@teamInfo')->name('teamInfo');
 Route::get('department', 'DepartmentController@department')->name('department');
 Route::get('/admin/team/moderator/add', 'TeamsController@returnView')->name('returnView');
+
 Route::post('/admin/team/moderator/add', 'TeamsController@addModerator')->name('moderatorAdd');
+
 Route::get('/users', 'UserController@userIdName')->name('usersIdName');
+
+
 });
