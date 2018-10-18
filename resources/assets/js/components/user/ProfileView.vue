244f5b3d47d718429bfbf3a3e3ee2e8c25154462
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-10-18 12:23:45 +0300

    tested some stuff out

--- resources/assets/js/components/user/ProfileView.vue
+++ resources/assets/js/components/user/ProfileView.vue
@@ -1,132 +1,151 @@
 <template>
-<div>
-    <div class="profile">
-        <div class="row">
-            <div class="col-sm-12 col-md-12 col-lg-4">
-                <profile-avatar :image="user.image" :user-id="user.id"></profile-avatar>
-            </div>
-            <div class="col-sm-12 col-md-12 col-lg-8">
-                <h1 class="profile__name">{{user.name}}</h1>
-                <div class="profile__role">
-                        <span>Junior UI designer</span><i class="la la-angle-double-right hidden-xs"></i><i class="la la-angle-double-down hidden-lg hidden-md hidden-sm"></i><span
-                            class="profile__role--current">UX Arhitekt</span><i class="la la-angle-double-right hidden-xs"></i><i class="la la-angle-double-down hidden-lg hidden-md hidden-sm"></i><span>Team lead</span>
+    <div>
+        <div class="profile">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-4">
+                    <profile-avatar :image="user.image" :user-id="user.id"></profile-avatar>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-8">
+                    <h1 class="profile__name">{{user.name}}</h1>
+                    <div class="profile__role">
+                        <span>Junior UI designer</span><i class="la la-angle-double-right hidden-xs"></i><i
+                            class="la la-angle-double-down hidden-lg hidden-md hidden-sm"></i><span
+                            class="profile__role--current">UX Arhitekt</span><i
+                            class="la la-angle-double-right hidden-xs"></i><i
+                            class="la la-angle-double-down hidden-lg hidden-md hidden-sm"></i><span>Team lead</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-12 col-md-4">
-            <h3 class="profile__subtitle">Profile</h3>
-            <div class="profile__details">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <div class="profile__details--title" >Department: </div>
-                    </div>
-                    <div class="col-xs-9">
-                        <div class="profile__details--info"
-                             @click="changeText('department')"
-                        >
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+                <h3 class="profile__subtitle">Profile</h3>
+                <div class="profile__details">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <div class="profile__details--title">Department:</div>
+                        </div>
+                        <div class="col-xs-9">
+                            <div class="profile__details--info"
+                                 @click="changeText('department')"
+                            >
                             <span v-if="user.department">
                                 {{user.department}}
                                 </span>
-                            <span v-else>Choose department</span>
-                        </div>
-                        <user-department
-                                @close="onClose"
-                                v-show="canedit && editdepartment"
-                                :userdata="userdata"
-                                @select-updated="personalDataUpdated('department', $event)"
-                        ></user-department>
+                                <span v-else>Choose department</span>
+                            </div>
+                            <user-department
+                                    @close="onClose"
+                                    v-show="canedit && editdepartment"
+                                    :userdata="userdata"
+                                    @select-updated="personalDataUpdated('department', $event)"
+                            ></user-department>
 
-                        <user-team
-                                v-show="canedit && editdepartment"
-                                :userid="user.id"
-                                :userdata="userdata"
-                                @select-updated="personalDataUpdated('team', $event)"
-                        ></user-team>
+                            <user-team
+                                    v-show="canedit && editdepartment"
+                                    :userid="user.id"
+                                    :userdata="userdata"
+                                    @select-updated="personalDataUpdated('team', $event)"
+                            ></user-team>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-3">
-                        <div class="profile__details--title"  >Phone:</div>
-                    </div>
-                    <div class="col-xs-9">
-                        <editable-input-component
-                                v-if="editphone"
-                                :endpoint="'/user/'+ user.id + '/update/phone'"
-                                :data="user.phone"
-                                ref="phone"
-                                element-name="PHONE"
-                                @input-updated="personalDataUpdated('phone', $event)"
-                                @canceled="hideInput('phone', $event)"
-                        ></editable-input-component>
-                        <div class="profile__details--info" v-if="editphone === false"
-                              @dblclick="changeText('phone')">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <div class="profile__details--title">Phone:</div>
+                        </div>
+                        <div class="col-xs-9">
+                            <editable-input-component
+                                    v-if="editphone"
+                                    :endpoint="'/user/'+ user.id + '/update/phone'"
+                                    :data="user.phone"
+                                    ref="phone"
+                                    element-name="PHONE"
+                                    @input-updated="personalDataUpdated('phone', $event)"
+                                    @canceled="hideInput('phone', $event)"
+                            ></editable-input-component>
+                            <div class="profile__details--info" v-if="editphone === false"
+                                 @dblclick="changeText('phone')">
                                <span v-if="user.phone">
                                 {{user.phone}}
                                 </span>
-                            <span v-else>Enter Phone number</span>
+                                <span v-else>Enter Phone number</span>
 
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="row">
-                    <div class="col-xs-3">
-                        <div class="profile__details--title">Email:</div>
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <div class="profile__details--title">Email:</div>
+                        </div>
+                        <div class="col-xs-9">
+                            <div class="profile__details--info">
+                                {{user.email}}
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-xs-9">
-                        <div class="profile__details--info">
-                            {{user.email}}</div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-3">
-                        <div class="profile__details--title">Skype:</div>
-                    </div>
-                    <div class="col-xs-9">
-                        <editable-input-component
-                                v-if="editskype"
-                                :endpoint="'/user/' + user.id + '/update/skype'"
-                                :data="user.skype"
-                                element-name="SKYPE"
-                                @input-updated="personalDataUpdated('skype', $event)"
-                                @canceled="hideInput('skype', $event)"
-                        ></editable-input-component>
-                        <div class="profile__details--info" v-if="editskype === false"
-                             @dblclick="changeText('skype')">
+                    <div class="row">
+                        <div class="col-xs-3">
+                            <div class="profile__details--title">Skype:</div>
+                        </div>
+                        <div class="col-xs-9">
+                            <editable-input-component
+                                    v-if="editskype"
+                                    :endpoint="'/user/' + user.id + '/update/skype'"
+                                    :data="user.skype"
+                                    element-name="SKYPE"
+                                    @input-updated="personalDataUpdated('skype', $event)"
+                                    @canceled="hideInput('skype', $event)"
+                            ></editable-input-component>
+                            <div class="profile__details--info" v-if="editskype === false"
+                                 @dblclick="changeText('skype')">
                             <span v-if="user.skype">
                                 {{user.skype}}
                                 </span>
-                            <span v-else>Enter Skype</span>
+                                <span v-else>Enter Skype</span>
 
+                            </div>
                         </div>
                     </div>
+                    <div class="profile__details--pills">
+                        <div class="profile__pill"><i class="la la-birthday-cake"></i> {{user.birthday}}</div>
+                        <div class="profile__pill"><img alt="" src="" class="profile__pill--logo"> 3y4m</div>
+                    </div>
+                    <div class="profile__details--text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.
+                            Proin gravida
+                            dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor.
+                            Sociis natoque
+                            penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla
+                            luctus pharetra
+                            vulputate, felis tellus mollis orci, sed rhoncus pronin sapien nunc accuan eget.</p>
+                    </div>
                 </div>
-                <div class="profile__details--pills">
-                    <div class="profile__pill"><i class="la la-birthday-cake"></i> {{user.birthday}}</div>
-                    <div class="profile__pill"><img alt="" src="" class="profile__pill--logo"> 3y4m</div>
+
+                <h3 class="profile__subtitle">STRENGTHS</h3>
+                <div class="profile__strenghts" >
+                    <user-strenght
+                            :canedit="canedit"
+                            :userid="user.id"
+                            v-for="strength in userStrengths"
+                            :strength="strength"
+                            :key="userStrengths.id"
+                    >
+                    </user-strenght>
                 </div>
-                <div class="profile__details--text">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida
-                        dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar sic tempor. Sociis natoque
-                        penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra
-                        vulputate, felis tellus mollis orci, sed rhoncus pronin sapien nunc accuan eget.</p>
+
+                    <user-children :canedit="canedit" :userid="user.id"></user-children>
+                    <user-hobbies :canedit="canedit" :userid="user.id"></user-hobbies>
+                    <user-badge :canedit="canedit" :userid="user.id"></user-badge>
+                </div>
+                <div class="col-sm-12 col-md-8">
+                    <user-role :canedit="canedit" :userid="user.id"></user-role>
+                    <user-skill :canedit="canedit" :userid="user.id"></user-skill>
+                    <user-project :canedit="canedit" :userid="user.id"></user-project>
+                    <user-training :canedit="canedit" :userid="user.id"></user-training>
                 </div>
             </div>
-            <user-strenght :canedit="canedit" :userid="user.id" ></user-strenght>
-            <user-children :canedit="canedit" :userid="user.id"></user-children>
-            <user-hobbies :canedit="canedit" :userid="user.id"></user-hobbies>
-            <user-badge :canedit="canedit" :userid="user.id"></user-badge>
         </div>
-        <div class="col-sm-12 col-md-8">
-            <user-role :canedit="canedit" :userid="user.id"></user-role>
-            <user-skill :canedit="canedit" :userid="user.id"></user-skill>
-            <user-project :canedit="canedit" :userid="user.id"></user-project>
-            <user-training :canedit="canedit" :userid="user.id"></user-training>
-        </div>
-    </div>
-</div>
 </template>
 
 
@@ -137,11 +156,11 @@
 
     export default {
         name: "ProfileView",
-        components: { EditableInputComponent},
+        components: {EditableInputComponent},
         props: ['user', 'image'],
-        data: function(){
-            return{
-                edit:false,
+        data: function () {
+            return {
+                edit: false,
                 userdata: '',
                 department: 0,
                 text: '',
@@ -150,6 +169,18 @@
                 editteam: false,
                 editphone: false,
                 editskype: false,
+
+                userStrengths: [
+
+                    {id: 0, description: 'hello world!', strength_name: 'IDEATION', value: 10,},
+                    {id: 1, strength_name: 'COMMUNICATION', value: 20, description: 'hello mind!'},
+                    {id: 2, strength_name: 'WOO', value: 50, description: 'hello mind!'},
+                    {id: 3, strength_name: 'STRATEGIC', value: 60, description: 'hello mind!'},
+                    {id: 4, strength_name: 'FUTURISTIC', value: 75, description: 'hello mind!'},
+                    {id: 5, strength_name: 'Input', value: 20, description: 'hello mind!'},
+
+                ]
+
             }
         },
         mounted() {
@@ -158,9 +189,9 @@
 
             if (authUser.id === this.user.id
                 || Vue.$isAdmin()
-                || Vue.$canModerateTeam(this.userdata.team_id)){
+                || Vue.$canModerateTeam(this.userdata.team_id)) {
 
-                 this.canedit = true;
+                this.canedit = true;
             }
 
         },
@@ -169,7 +200,7 @@
 
             onClose(value) {
 
-                this.editdepartment=this.value;
+                this.editdepartment = this.value;
             },
             changeText(type) {
                 if (this.canedit) {
--- resources/assets/js/components/user/UserStrenght.vue
+++ resources/assets/js/components/user/UserStrenght.vue
@@ -1,75 +1,65 @@
 <template>
     <div>
-        <h3 class="profile__subtitle">Strenghts</h3>
-        <div class="profile__strenghts">
-            <div class="profile__strenght">
-                <div class="profile__strenght--info">
-                    <i class="la la-lightbulb-o"></i> IDEATION
-                </div>
-                <div class="progress m-progress--custom">
-                    <div class="progress-bar m--bg-info" role="progressbar" style="width: 90%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
+
+        <div class="profile__strenght" @click="showInput()">
+            <div class="profile__strenght--info">
+                <i class="la la-lightbulb-o" ></i> {{this.userStrength.strength_name}}
             </div>
-            <div class="profile__strenght">
-                <div class="profile__strenght--info">
-                    <i class="la la-bullhorn"></i> COMMUNICATION
-                </div>
-                <div class="progress m-progress--custom">
-                    <div class="progress-bar m--bg-info" role="progressbar" style="width: 85%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
+            <div class="progress m-progress--custom" >
+                <div class="progress-bar m--bg-info"  role="progressbar"
+                     :style="{width:this.progressValue+'%'}"
+                     aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+
             </div>
-            <div class="profile__strenght">
-                <div class="profile__strenght--info">
-                    <i class="la la-bullhorn"></i> WOO
-                </div>
-                <div class="progress m-progress--custom">
-                    <div class="progress-bar m--bg-info" role="progressbar" style="width: 75%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-            </div>
-            <div class="profile__strenght">
-                <div class="profile__strenght--info">
-                    <i class="la la-crosshairs"></i> STRATEGIC
-                </div>
-                <div class="progress m-progress--custom">
-                    <div class="progress-bar m--bg-info" role="progressbar" style="width: 70%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-            </div>
-            <div class="profile__strenght">
-                <div class="profile__strenght--info">
-                    <i class="la la-flask"></i> FUTURISTIC
-                </div>
-                <div class="progress m-progress--custom">
-                    <div class="progress-bar m--bg-info" role="progressbar" style="width: 60%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-            </div>
-            <div class="profile__strenght">
-                <div class="profile__strenght--info">
-                    <i class="la la-flask"></i> Input
-                </div>
-                <div class="progress m-progress--custom">
-                    <div class="progress-bar m--bg-info" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-            </div>
+            <input v-show="admin && show" type="range" v-model="progressValue"
+                   @change="showValue(progressValue,userStrength.id)">
 
         </div>
+
     </div>
 </template>
 
 <script>
     export default {
         name: "UserStrenght",
-        props:['userid'],
-        data(){
+        props: ['userid', 'strength'],
+        data() {
             return {
 
-                userId:'',
+                progressValue: '',
+                currentUser: '',
+                show: false,
+                admin: false,
+                test: null,
+                userStrength: {},
+
 
             }
         },
-        mounted(){
-            this.userId=this.userid;
+        mounted() {
+            this.currentUser = this.userid;
+            this.checkAdmin();
+            this.userStrength = this.strength;
+            this.progressValue = this.strength.value;
 
-        }
+
+        },
+        methods: {
+            showInput() {
+                console.log('hey');
+                    this.show === false ? this.show = true : this.show = false;
+
+            },
+            checkAdmin() {
+                Vue.$isAdmin() ? this.admin = true : this.admin = false;
+            },
+            showValue(value, id) {
+                console.log(value + ' ' + id);
+            }
+
+
+        },
+
     }
 </script>
 
