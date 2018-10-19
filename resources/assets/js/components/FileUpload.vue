50dc2e815e0af3225db5962b9bb43e9b0d54c558
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-10-19 11:38:37 +0300

    Chaning picture is now nicer

--- resources/assets/js/components/FileUpload.vue
+++ resources/assets/js/components/FileUpload.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
-        <input id="imageChoose" type="file" v-on:change="onFileChange" class="form-control">
-        <button class="btn btn-success btn-block" @click="upload">SAVE</button>
+        <div class="profile__avatar--overlay">
+            <div class="profile__avatar--action"><span class="btn m-btn--pill btn-success fileinput-button"><i
+                    class="la la-plus"></i> <span> Add image... </span> <input id="imageChoose" type="file" v-on:change="onFileChange"></span>
+                <button @click="upload" type="submit" class="btn m-btn--pill btn-info"><i class="la la-upload"></i>
+                    <span> Upload image </span></button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,19 +14,19 @@
     export default {
         name: "FileUpload",
         props: ['endpoint'],
-        data(){
+        data() {
             return {
                 image: ''
             }
         },
         methods: {
-            onFileChange: function(e) {
+            onFileChange: function (e) {
                 let files = e.target.files || e.dataTransfer.files;
                 if (!files.length)
                     return;
                 this.createImage(files[0]);
             },
-            createImage: function(file) {
+            createImage: function (file) {
 
                 let reader = new FileReader();
                 let vm = this;
@@ -30,9 +35,9 @@
                 };
                 reader.readAsDataURL(file);
             },
-            upload: function(){
+            upload: function () {
                 let vm = this;
-                axios.post(this.endpoint,{image: this.image}).then(response => {
+                axios.post(this.endpoint, {image: this.image}).then(response => {
                     vm.$emit('file-uploaded', response);
                 });
             }
