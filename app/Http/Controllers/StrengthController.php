60f292d28183d7ae81277df5aed048496bfe02f6
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-10-19 11:40:44 +0300

    strength controller

---
+++ app/Http/Controllers/StrengthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StrengthController extends Controller
+{
+    public function view()
+    {
+        return view('strength.strength');
+    }
+}
