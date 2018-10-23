28220232c225352879a6c08eebc69c876e95b134
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-10-23 10:04:36 +0300

    view to Create strength and category

--- resources/views/strength/strength.blade.php
+++ resources/views/strength/strength.blade.php
@@ -5,7 +5,7 @@
     <div class="d-flex align-items-center">
         <div class="mr-auto">
             <h3 class="m-subheader__title ">
-                Strength View
+                Create Category and Strength
             </h3>
         </div>
         <div>
@@ -25,37 +25,90 @@
 
 @section('content-body')
 
-    {{csrf_field()}}
     <div class="m-content">
         <div id="app">
-            <div class="admin__form admin__form--clear"><h4>Department</h4>
-                <form class="m-form">
-                    <div class="m-portlet__body">
-                        <div class="form-group m-form__group row"><label for="example-text-input"
-                                                                         class="col-sm-3 col-xs-12  col-form-label">Strength</label>
-                            <div class="col-sm-9 col-xs-12 "><input type="text" placeholder="Strength name"
-                                                                    class="form-control m-input"></div>
+            <div class="row">
+                <div class="admin__form admin__form--clear col-lg-6"><h4>Category</h4>
+                    <form class="m-form" action="{{action('CategoryController@createCategory')}}" method="post">
+                        {{csrf_field()}}
+                        <div class="m-portlet__body">
+                            <div class="form-group m-form__group row"><label for="example-text-input"
+                                                                             class="col-sm-3 col-xs-12  col-form-label">Category Name</label>
+                                <div class="col-sm-9 col-xs-12 "><input type="text" placeholder="Category name"
+                                                                        class="form-control m-input" name="category_name"></div>
+                            </div>
+                            <div class="form-group m-form__group row"><label for="example-text-input"
+                                                                             class="col-sm-3 col-xs-12  col-form-label">Category Colour</label>
+                                <div class="col-sm-9 col-xs-12 "><input type="color" placeholder="Category colour E.g blue"
+                                                                        class="form-control m-input" name="category_colour"></div>
+                            </div>
+                            <div class="form-group m-form__group row"><label for="exampleTextarea"
+                                                                             class="col-sm-3 col-xs-12 col-form-label">Description</label>
+                                <div class="col-sm-9 col-xs-12"><textarea id="exampleTextarea" rows="3"
+                                                                          class="form-control m-input" name="category_description"></textarea></div>
+                            </div>
                         </div>
-                        <div class="form-group m-form__group row"><label for="exampleTextarea"
-                                                                         class="col-sm-3 col-xs-12 col-form-label">Description</label>
-                            <div class="col-sm-9 col-xs-12"><textarea id="exampleTextarea" rows="3"
-                                                                      class="form-control m-input"></textarea></div>
-                        </div>
-                    </div>
-                    <div class="m-portlet__foot m-portlet__foot--fit">
-                        <div class="m-form__actions">
-                            <div class="row">
-                                <div class="col-sm-3 col-xs-12"></div>
-                                <div class="col-sm-9 col-xs-12">
-                                    <div class="profile-timeline__action">
-                                        <button href="" class="btn btn-success m-btn m-btn--pill">
-                                            <span><span>Submit</span></span></button>
+                        <div class="m-portlet__foot m-portlet__foot--fit">
+                            <div class="m-form__actions">
+                                <div class="row">
+                                    <div class="col-sm-3 col-xs-12"></div>
+                                    <div class="col-sm-9 col-xs-12">
+                                        <div class="profile-timeline__action">
+                                            <button type="submit"  class="btn btn-success m-btn m-btn--pill">
+                                                <span><span>Submit</span></span></button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
+
+                <div class="admin__form admin__form--clear col-lg-6"><h4>Strength</h4>
+                    <form class="m-form" method="post" action="{{action('StrengthController@createStrength')}}">
+                        {{csrf_field()}}
+                        <div class="m-portlet__body">
+                            <div class="form-group m-form__group row"><label for="example-text-input"
+                                                                             class="col-sm-3 col-xs-12  col-form-label">Strength</label>
+                                <div class="col-sm-9 col-xs-12 "><input type="text" name="strength_name" placeholder="Strength name"
+                                                                        class="form-control m-input"></div>
+                            </div>
+                            <div class="form-group m-form__group row"><label for="exampleTextarea"
+                                                                             class="col-sm-3 col-xs-12 col-form-label">Category</label>
+                                <div class="col-sm-9 col-xs-12"><select id="exampleTextarea" rows="3"
+                                                                        class="form-control m-input" name="category_id" >
+                                        <option ></option>
+                                        @foreach($categories as $category)
+                                        <option value="{{$category->id}}">{{$category->category_name}}</option>
+                                        @endforeach
+
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group m-form__group row"><label for="exampleTextarea"
+                                                                             class="col-sm-3 col-xs-12 col-form-label">Description</label>
+                                <div class="col-sm-9 col-xs-12"><textarea id="exampleTextarea" rows="3"
+                                                                          class="form-control m-input" name="strength_description"></textarea></div>
+                            </div>
+
+                        </div>
+                        <div class="m-portlet__foot m-portlet__foot--fit">
+                            <div class="m-form__actions">
+                                <div class="row">
+                                    <div class="col-sm-3 col-xs-12"></div>
+                                    <div class="col-sm-9 col-xs-12">
+                                        <div class="profile-timeline__action">
+                                            <button type="submit" class="btn btn-success m-btn m-btn--pill">
+                                                <span><span>Submit</span></span></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+
             </div>
         </div>
     </div>
