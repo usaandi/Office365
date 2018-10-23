6aedd6cdd4f54a897ea9c234b9919382d4c81a33
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-10-23 10:05:08 +0300

    creates category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    public function createCategory(Request $request)
+    {
+
+        try {
+
+            $data = $request->all();
+
+            $rules = [
+                'category_name' => 'required|alpha_num',
+                'category_description' => 'required|string',
+                'category_colour' => 'required|string',
+            ];
+            $validator = Validator::make($data, $rules);
+
+            if ($validator->passes()) {
+
+                $categoryName =ucfirst(strtolower($data['category_name']));
+                $categoryColour =strtolower($data['category_colour']);
+
+                $category = Category::where('category_name', $categoryName)
+                ->get();
+
+                if (!$category->isEmpty()) {
+                    return 'duplicate';
+                }
+                else {
+                    $category = Category::create([
+                        'category_name'=> $categoryName,
+                        'category_description' => $data['category_description'],
+                        'category_colour' => $categoryColour,
+                    ]);
+                    return redirect()->back();
+
+                }
+
+            }
+
+
+        } catch (\Exception $e) {
+
+        }
+
+    }
+}
