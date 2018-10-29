a01e4edee3e281ea81eab948598127997eacd56b
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-10-29 15:34:47 +0200

    assign strength to user and update it

--- app/Http/Controllers/UserStrengthController.php
+++ app/Http/Controllers/UserStrengthController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Validation\Validator;
+use App\User;
+use Validator;
 use Illuminate\Http\Request;
 
 class UserStrengthController extends Controller
@@ -10,13 +11,46 @@ class UserStrengthController extends Controller
     public function userStrength(Request $request, $id)
     {
         try {
-            $data = $request->all();
+            $strengths = $request->all();
 
-            $rules = [];
+            $rules = [
+                '*.strength_id' => 'required|integer',
+                '*.strength_name' => 'required|string',
+                '*.strength_index' => 'required|integer',
+            ];
 
-            $validator=Validator::make($data,$rules);
+            $validator = Validator::make($strengths, $rules);
 
-            if($validator->passes()){
+            if ($validator->passes()) {
+
+                $user = User::findOrFail($id);
+
+                $userStrength = $user->userStrength()->get();
+
+                if ($userStrength->isEmpty()) {
+                    foreach ($strengths as $strength) {
+                        $user->userStrength()->create([
+                            'user_id' => $id,
+                            'strength_id' => $strength['strength_id'],
+                            'rank' => $strength['strength_index'],
+                        ]);
+                    }
+
+                } else {
+
+                    $userStrengths = $user->userStrength()->get();
+                   for ($i = 0; $i < count($userStrengths); $i++){
+
+                       $userStrengths[$i]->update([
+                          'strength_id' => $strengths[$i]['strength_id'],
+                          'user_id' => $id,
+                          'rank' => $strengths[$i]['strength_index'],
+                       ]);
+
+                   }
+                    return redirect()->back();
+
+                }
 
 
             }
