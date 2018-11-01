40c9cf49c06cc497effebeaa455ea34bb32ee45d
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-11-01 16:24:15 +0200

    table view for each category strength department user

--- resources/assets/js/components/UsersStrengthView/MainView.vue
+++ resources/assets/js/components/UsersStrengthView/MainView.vue
@@ -4,125 +4,33 @@
             <thead>
             <tr>
                 <th></th>
-                <th v-for="catestre in categoryStrength" :colspan="9"  scope="colgroup" :id="catestre.category_id">
-                    <span>{{catestre.category_name}}</span>
+                <th v-for="category in categories" :colspan="category.strengths.length" scope="colgroup"
+                    :id="category.category_id">
+                    <span>{{category.category_name}}</span>
                 </th>
-
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <td></td>
-                <td scope="col"><span>Strength 1</span></td>
-                <td scope="col"><span>strength 2</span></td>
-                <td scope="col"><span>strength 3</span></td>
-                <td scope="col"><span>strength 4</span></td>
-                <td scope="col"><span>strength 5</span></td>
-                <td scope="col"><span>strength 6</span></td>
-                <td scope="col"><span>Self-accurance</span></td>
-                <td scope="col"><span>Individualization</span></td>
-                <td scope="col"><span>strength 9</span></td>
+                <tr>
+                    <td></td>
+                    <td
+                            v-for="strength in strengths"
+                            :id="strength.strength_id"
+                            :headers="strength.category_id"
+                    >
+                        <span>{{strength.strength_name}}</span>
+                    </td>
 
-                <td scope="col"><span>Strength 1</span></td>
-                <td scope="col"><span>strength 2</span></td>
-                <td scope="col"><span>strength 3</span></td>
-                <td scope="col"><span>strength 4</span></td>
-                <td scope="col"><span>strength 5</span></td>
-                <td scope="col"><span>strength 6</span></td>
-                <td scope="col"><span>strength 7</span></td>
-                <td scope="col"><span>strength 8</span></td>
-                <td scope="col"><span>strength 9</span></td>
-
-                <td scope="col"><span>Strength 1</span></td>
-                <td scope="col"><span>strength 2</span></td>
-                <td scope="col"><span>strength 3</span></td>
-                <td scope="col"><span>strength 4</span></td>
-                <td scope="col"><span>strength 5</span></td>
-                <td scope="col"><span>strength 6</span></td>
-                <td scope="col"><span>strength 7</span></td>
-                <td scope="col"><span>strength 8</span></td>
-                <td scope="col"><span>strength 9</span></td>
-
-                <td scope="col"><span>Strength 1</span></td>
-                <td scope="col"><span>strength 2</span></td>
-                <td scope="col"><span>strength 3</span></td>
-                <td scope="col"><span>strength 4</span></td>
-                <td scope="col"><span>strength 5</span></td>
-                <td scope="col"><span>strength 6</span></td>
-                <td scope="col"><span>strength 7</span></td>
-                <td scope="col"><span>strength 8</span></td>
-                <td scope="col"><span>strength 9</span></td>
-            </tr>
-            <tr>
-                <th scope="colgroup">Fenomen</th>
-            </tr>
-            <tr>
-                <td><span>User1</span></td>
-                <td><span>1</span></td>
-                <td></td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-            </tr>
-            <tr>
-                <td>User2</td>
-            </tr>
-            <tr>
-                <td>User3</td>
-            </tr>
-            <tr>
-                <td>User4</td>
-            </tr>
-            <tr>
-                <th scope="colgroup">Fenomen</th>
-            </tr>
-            <tr>
-                <td><span>User1</span></td>
-                <td><span>1</span></td>
-                <td><span></span></td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-            </tr>
-            <tr>
-                <td>User2</td>
-            </tr>
-            <tr>
-                <td>User3</td>
-            </tr>
-            <tr>
-                <td>User4</td>
-            </tr>
+                </tr>
+            </tbody>
+            <tbody v-for="department in departments">
+                <tr>
+                    <th :id="department.department_id" scope="colgroup"><span>{{department.department_name}}</span></th>
+                </tr>
+                <tr v-for="user in department.users">
+                    <td><span>{{ user.user_name }}</span></td>
+                    <td v-for="strength in strengths"><span>{{checkStrength(strength, user.strengths)}}</span></td>
+                </tr>
             </tbody>
         </table>
     </div>
@@ -130,38 +38,54 @@
 
 <script>
     import axios from 'axios';
+
     export default {
         name: "department",
-        data(){
-            return{
-                categoryStrength:'',
+        data() {
+            return {
+                categories: '',
+                departments: '',
+                strengths: [],
             }
         },
-        mounted(){
-          this.fetch();
+        mounted() {
+            this.fetchCategories();
+            this.fetchDepartment();
         },
-        methods:{
-            fetch(){
-                axios.get('test1').then(response => {
-                   this.categoryStrength=response.data;
+        methods: {
+            fetchCategories() {
+                axios.get('categories').then(response => {
+                    this.categories = response.data;
+                    this.loopThrough(this.categories);
                 });
             },
-            countSpan(){
+            fetchDepartment() {
+                axios.get('departments').then(response => {
+                    this.departments = response.data;
 
-                   // return this.categoryStrength.length
+                })
+            },
 
+            loopThrough(categories) {
+                for (let i = 0; i < categories.length; i++) {
+                    this.strengths = this.strengths.concat(categories[i].strengths);
+                }
+            },
+
+            checkStrength(strength, userStrengths) {
+                for(let i = 0; i < userStrengths.length; i++ ){
+                    if(strength.strength_id === userStrengths[i].strength_id){
+                        return userStrengths[i].strength_rank;
+                    }
+                }
+                return '';
             },
         }
     }
 </script>
 
 <style scoped>
-    .rotate {
 
-        transform: rotate(90deg);
-        padding-top: 15px;
-        padding-bottom: 15px;
-    }
 
 
 </style>
