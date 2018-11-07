a49278e309a4aacfa1ef7d0fd19df36d8f6c9581
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-11-07 15:37:19 +0200

    Modal that can be reused everywhere

---
+++ resources/assets/js/components/warning/modal.vue
@@ -0,0 +1,43 @@
+<template>
+   <script type="text/x-template" id="modal-template">
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              default footer
+              <button class="modal-default-button" @click="$emit('close')">
+                OK
+              </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</script>
+</template>
+
+<script>
+    export default {
+        name: "WarningModal"
+    }
+</script>
+
+<style scoped>
+
+</style>
