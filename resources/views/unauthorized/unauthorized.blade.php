88a2f0d2a2e555d9404725d72ad632228175e82e
Kaur Joonas Usai <kaurjoonas@hot.ee>
2018-11-15 12:18:23 +0200

    ss

--- resources/views/unauthorized/unauthorized.blade.php
+++ resources/views/unauthorized/unauthorized.blade.php
@@ -3,4 +3,10 @@
     @isset($error)
         <p>{{$error}}</p>
         @endisset
+    <div class="col-xs-9">
+        <div class="profile__details--info">
+            <a href="/home"><span><button
+                            class="btn btn-success">Main Page</button></span></a>
+        </div>
+    </div>
 </div>
