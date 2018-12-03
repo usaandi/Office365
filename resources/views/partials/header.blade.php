67e42db9457b18d2e1f8719f26e4976e9ab9012d
Kaur <kaurusai@gmail.com>
2018-12-03 10:26:20 +0200

    fixed tab not staying the way it should

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -240,7 +240,7 @@
                     <div id="m_header_menu"
                          class="m-header-menu m-aside-header-menu-mobile m-aside-header-menu-mobile--offcanvas  m-header-menu--skin-dark m-header-menu--submenu-skin-light m-aside-header-menu-mobile--skin-light m-aside-header-menu-mobile--submenu-skin-light ">
                         <ul class="m-menu__nav  m-menu__nav--submenu-arrow ">
-                            <li class="m-menu__item m-menu__item--submenu m-menu__item--tabs m-menu__item--open-dropdown m-menu__item--hover"
+                            <li class="m-menu__item m-menu__item--submenu m-menu__item--tabs m-menu__item--open-dropdown {{ Request::is('admin/*') ? '' : 'm-menu__item--hover'}}"
                                 m-menu-submenu-toggle="tab" aria-haspopup="true">
 
                                 <a href="/" class="m-menu__link m-menu__toggle">
@@ -277,7 +277,7 @@
                                 </div>
                             </li>
                             @hasanyrole('Admin')
-                            <li class="m-menu__item m-menu__item--submenu m-menu__item--tabs m-menu__item--open-dropdown"
+                            <li class="m-menu__item m-menu__item--submenu m-menu__item--tabs m-menu__item--open-dropdown {{ Request::is('admin/*') ? 'm-menu__item--hover' : ''}}"
                                 m-menu-submenu-toggle="tab"
                                 aria-haspopup="true"><a href="javascript:;" class="m-menu__link m-menu__toggle"
                                                         title="Non functional dummy link"><span
