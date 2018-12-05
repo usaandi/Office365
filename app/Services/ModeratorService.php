384cba6863169ff9e79258873d9759920d8cc929
Kaur <kaurusai@gmail.com>
2018-12-05 13:22:09 +0200

    New Service registation that checks if user is this team moderator

---
+++ app/Services/ModeratorService.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaur-joonas
+ * Date: 5.12.18
+ * Time: 12:57
+ */
+
+namespace App\Services;
+
+use App\UserTeamModerator;
+
+class Moderator
+{
+    /**
+     *
+     *  check if user is team moderator
+     *
+     * @param int Team ID
+     * @param int User ID
+     * @return boolean
+     *
+     */
+
+    function isModerator($teamId, $userId)
+    {
+        $moderatedTeams = UserTeamModerator::where('user_id', $userId)->get(['team_id'])->toArray();
+        for ($i = 0; $i < count($moderatedTeams); $i++) {
+            if (in_array($teamId, $moderatedTeams[$i])) {
+                return TRUE;
+
+            }
+        }
+        return FALSE;
+    }
+}
