5a9d7f27ea21587d8e270929652233e99a6eefd4
Kaur <kaurusai@gmail.com>
2018-12-05 13:34:12 +0200

    return view with info

--- app/Http/Controllers/AdminTeamController.php
+++ app/Http/Controllers/AdminTeamController.php
@@ -2,21 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ModeratorService;
+use App\UserTeamModerator;
 use Illuminate\Http\Request;
 use App\Team;
+use App\User;
+use phpDocumentor\Reflection\Types\Boolean;
+
 
 class AdminTeamController extends Controller
 {
-    public function index($id)
+
+    public function index($id, ModeratorService $moderatorService)
     {
         try {
 
             $auth = \Auth::user();
             $this->authorize('admin', $auth);
+            $teamArray = [];
 
-            $team = Team::findOrFail($id)->users()->get();
+            $team = Team::findOrFail($id);
+            $teamArray = [
+                'team_name' => $team->team_name,
+                'team_id' => $team->id
+            ];
+            $teamUsers = $team->users()->get();
 
-            return view('admin.adminTeamView', with(['id' => $id]));
+            $isModerator = null;
+
+            if ($teamUsers->isNotEmpty()) {
+
+                foreach ($teamUsers as $user) {
+
+                    $isModerator =  $moderatorService->isModerator($id,$user->id);
+
+                    $teamArray['users'][] = [
+                        'user_name' => $user->name,
+                        'user_id' => $user->id,
+                        'team_moderator' => $isModerator,
+                    ];
+
+
+                }
+            } else {
+                $teamArray ['users'] = [];
+            }
+            return view('admin.adminTeamView', with(['id' => $id, 'team' => $teamArray]));
 
         } catch (\Exception $e) {
         }
