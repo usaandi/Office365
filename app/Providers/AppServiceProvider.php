8ce7035ca9d5c0b80be6c76ea4742d9198cd1294
Kaur <kaurusai@gmail.com>
2018-12-05 13:34:34 +0200

    Registered ModeratorService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\ModeratorService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
     }
 
     /**
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(ModeratorService::class,function (){
+            return new ModeratorService();
+        });
     }
 }
