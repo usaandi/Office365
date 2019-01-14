9c4d246a217a992e10ba28fb37e4a8ea224d9ce9
Kaur <kaurusai@gmail.com>
2019-01-14 16:42:40 +0200

    inject data to view

--- app/Http/Controllers/DepartmentUserCategoryStrengthController.php
+++ app/Http/Controllers/DepartmentUserCategoryStrengthController.php
@@ -13,7 +13,11 @@ class DepartmentUserCategoryStrengthController extends Controller
     {
         try {
 
-            return view('ViewListOfStrengths');
+            $user = $this->departmentUserStrength();
+            $category = $this->categoryStrength();
+
+
+            return view('ViewListOfStrengths')->with(['user' => $user, 'category' => $category]);
 
         } catch (\Exception $e) {
         }
@@ -76,9 +80,8 @@ class DepartmentUserCategoryStrengthController extends Controller
                             'strength_name' => $strength['strength']['strength_name'],
                         ];
                     }
-                }
-                else {
-                    $strengthCategoryArray[$i]['strengths']=[];
+                } else {
+                    $strengthCategoryArray[$i]['strengths'] = [];
                 }
             }
 
