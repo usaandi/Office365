98283e8dc7e6c97585b47d4070ab31b14eab4c9b
Kaur <kaurusai@gmail.com>
2019-01-17 14:43:55 +0200

    route to get views

--- routes/web.php
+++ routes/web.php
@@ -130,6 +130,7 @@ Route::group(['middleware' => 'auth'], function () {
 
     //Notes
     Route::get('/user/{id}/career/note/{roleId}', 'NoteController@index')->name('userNote');
+    Route::get('/user/career/note/{roleId}/test', 'NoteController@noteInfo')->name('userNote');
 
 
 });
