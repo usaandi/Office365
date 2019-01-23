a4575c7a3ce9ad617f31eed237d37d8582922a9d
krister.joona <krister.joona.kalamagi@adm.ee>
2019-01-23 13:22:01 +0200

    Reworked uploading image

--- resources/assets/js/components/FileUpload.vue
+++ resources/assets/js/components/FileUpload.vue
@@ -4,19 +4,20 @@
             <div class="profile__avatar--action"><span class="btn m-btn--pill btn-success fileinput-button"><i
                     class="la la-plus"></i> <span> Add image... </span> <input id="imageChoose" type="file"
                                                                                v-on:change="onFileChange"></span>
-                <button @click="upload" type="submit" class="btn m-btn--pill btn-info"><i class="la la-upload"></i>
-                    <span> Upload image </span></button>
+                <button @click="deleteImage" type="submit" class="btn m-btn--pill btn-info"><i class="la la-upload"></i>
+                    <span> Delete image </span></button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
     import Vue from 'vue';
 
     export default {
         name: "FileUpload",
-        props: ['endpoint'],
+        props: ['endpoint', 'userId'],
         data() {
             return {
                 image: ''
@@ -35,19 +36,29 @@
                 let vm = this;
                 reader.onload = (e) => {
                     vm.image = e.target.result;
+                    this.upload();
                 };
                 reader.readAsDataURL(file);
+
+
             },
             upload: function () {
                 let vm = this;
-                if (vm.image === '') {
-                    vm.$emit('close', false);
-                } else {
-                    axios.post(this.endpoint, {image: this.image}).then(response => {
-                        vm.$emit('file-uploaded', response);
-                    });
-                }
+
+                axios.post(this.endpoint, {image: this.image}).then(response => {
+                    vm.$emit('file-uploaded', response);
+                });
+
             },
+            deleteImage() {
+
+                let vm = this;
+                axios.patch('adm/user/image/' + vm.userId).then(response => {
+                    vm.$emit('imageDelete', response);
+
+                });
+
+            }
 
         }
     }
