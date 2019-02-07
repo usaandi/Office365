9f1f7f1ce02f67d697120d079f391989ebc36e0a
Kaur <kaurusai@gmail.com>
2019-02-07 12:23:04 +0200

    Technology name and create technology logic

--- app/Technology.php
+++ app/Technology.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Technology extends Model
 {
-    //
+    protected $fillable = ['technology_name'];
+    protected $table = 'technologies';
+
+
+    public static function getTechnologyObjectByName($name)
+    {
+        return Technology::where('technology_name', $name)->first();
+    }
+
+    public static function createTechnology($name)
+    {
+        $technology = Technology::create([
+            'technology_name' => $name,
+        ]);
+
+        return $technology;
+    }
 }
