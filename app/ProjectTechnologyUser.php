9129ea7f0899909dc551f05597effdccea5d1c02
Kaur <kaurusai@gmail.com>
2019-02-07 12:23:25 +0200

    table and fillable fields

--- app/ProjectTechnologyUser.php
+++ app/ProjectTechnologyUser.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectTechnologyUser extends Model
 {
-    //
+    protected $table = 'projects_technologies_users';
+    protected $fillable = ['user_id', 'project_id', 'technologies_id'];
 }
